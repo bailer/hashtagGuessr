625743199157d24d4396856b513127cb2772315f
Jakob Sennerby <jakob.sennerby@gmail.com>
2015-12-02 00:32:35 +0100

    added a socket check

--- api/controllers/GameRoomController.js
+++ api/controllers/GameRoomController.js
@@ -30,30 +30,32 @@ module.exports = {
 
   // socket init
   initGameRoom: function (req, res) {
-    gameRoomId = req.param('id');
-    // console.log("Serving gameroom" + gameRoomId);
-    GameRoom.findOne(gameRoomId).populateAll().exec(function (err, found) {
-      if (found) {
-        GameRoom.subscribe(req, found.id);
-        // console.log("Found: %j", found);
-        if (found.players.length == 0) {
-          // console.log("Creating new player");
-          found.players.add({socketId: req.socket.id});
-          found.save(function (err, result) {
-            if (err) {
-              console.log(err);
-            } else {
-              return res.json({gameRoom: result, socketId: req.socket.id});
-            }
-          });
-          // Player.create({inGameRoom: gameRoomId, socketId: req.socket.id}, function (err, created) {
-          //   res.json(response);
-          // });
-        } else {
-          return res.json({gameRoom: found, socketId: req.socket.id});
+    if (req.isSocket) {
+      gameRoomId = req.param('id');
+      // console.log("Serving gameroom" + gameRoomId);
+      GameRoom.findOne(gameRoomId).populateAll().exec(function (err, found) {
+        if (found) {
+          GameRoom.subscribe(req, found.id);
+          // console.log("Found: %j", found);
+          if (found.players.length == 0) {
+            // console.log("Creating new player");
+            found.players.add({socketId: req.socket.id});
+            found.save(function (err, result) {
+              if (err) {
+                console.log(err);
+              } else {
+                return res.json({gameRoom: result, socketId: req.socket.id});
+              }
+            });
+            // Player.create({inGameRoom: gameRoomId, socketId: req.socket.id}, function (err, created) {
+            //   res.json(response);
+            // });
+          } else {
+            return res.json({gameRoom: found, socketId: req.socket.id});
+          }
         }
-      }
-    });
+      });
+    }
   },
 
   startGame: function(req, res) {
